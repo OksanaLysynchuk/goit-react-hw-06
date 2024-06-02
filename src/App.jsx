f61c4fdde313236c408348042982dc8b6b2cfa71
OksanaLysynchuk <138123244+OksanaLysynchuk@users.noreply.github.com>
2024-06-02 10:37:07 +0300

    fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,15 @@
-import { useDispatch, useSelector } from "react-redux";
-import { selectContacts, addContact } from "./redux/contactsSlice";
-import { changeFilter } from "./redux/filtersSlice.js";
 import ContactForm from "./components/ContactForm/ContactForm.jsx";
 import SearchBox from "./components/SearchBox/SearchBox.jsx";
 import ContactList from "./components/ContactList/ContactList.jsx";
 import "./App.css";
 
 function App() {
-  const dispatch = useDispatch();
-  const contacts = useSelector(selectContacts);
-  const filter = useSelector((state) => state.filters?.name || "");
-
-  const handleAddContact = (newContact) => {
-    dispatch(addContact(newContact));
-  };
-
-  const handleFilterChange = (filterValue) => {
-    dispatch(changeFilter(filterValue));
-  };
-
-  const filteredContacts = contacts.filter(
-    (contact) =>
-      contact.name && contact.name.toLowerCase().includes(filter.toLowerCase())
-  );
-
   return (
     <div className={CSS.container}>
       <h1>Phonebook</h1>
-      <ContactForm onAdd={handleAddContact} />
-      <SearchBox onFilter={handleFilterChange} />
-      <ContactList contacts={filteredContacts} />
+      <ContactForm />
+      <SearchBox />
+      <ContactList />
     </div>
   );
 }
--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -3,13 +3,19 @@ import { selectContacts } from "../../redux/contactsSlice.js";
 import CSS from "./ContactList.module.css";
 import Contact from "../Contact/Contact.jsx";
 
-export default function ContactList({ onDelete }) {
+export default function ContactList() {
   const contacts = useSelector(selectContacts);
+  const filter = useSelector((state) => state.filters?.name || "");
+  const filteredContacts = contacts.filter(
+    (contact) =>
+      contact.name && contact.name.toLowerCase().includes(filter.toLowerCase())
+  );
+
   return (
     <ul>
       {contacts.map((contact) => (
         <li className={CSS.listitem} key={contact.id}>
-          <Contact data={contact} onDelete={onDelete} />
+          <Contact data={contact} contacts={filteredContacts} />
         </li>
       ))}
     </ul>
